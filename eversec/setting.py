031ea1518a9cf2d55d650e27a6028c835e8f59ca
2059174599 <2059174599@qq.com>
2021-06-21 10:12:53 +0800

    douyin demo

---
+++ eversec/setting.py
@@ -0,0 +1,56 @@
+
+URLS = {
+    # 热搜
+    'hot_url': 'https://aweme-hl.snssdk.com/aweme/v1/hot/search/list/',
+    # 点赞 评论
+    'comment_url': 'https://aweme-hl.snssdk.com/aweme/v1/hot/search/video/list/?hotword=',
+    # 个人 主页
+    'homepage': 'https://www.iesdouyin.com/web/api/v2/user/info/?sec_uid=',
+}
+
+# 抖音 热搜
+DY_URLS = {
+    'hot_url' : 'https://tophub.today/n/K7GdaMgdQy',
+    'ir_urlname_key' : 'https://www.amemv.com/share/video/6974611041405717764/?mid=6974611041405717764',
+    'dy_share_url' : 'https://aweme.snssdk.com/aweme/v1/aweme/detail/',
+    'dy_person_url' : 'https://www.iesdouyin.com/web/api/v2/user/info/',
+    'paraments' : {
+        'origin_type': 'web',
+         'request_source': '0',
+         'is_story': '0',
+         'latitude': '28.00125',
+         'longitude': '105.563582',
+         'recommend_collect_feedback': '0',
+         'os_api': '22',
+         'device_type': 'SM-G973N',
+         'ssmix': 'a',
+         'manifest_version_code': '160201',
+         'dpi': '240',
+         'uuid': '351564515119295',
+         'app_name': 'aweme',
+         'version_name': '16.2.0',
+         'cpu_support64': 'false',
+         'app_type': 'normal',
+         'appTheme': 'dark',
+         'ac': 'wifi',
+         'host_abi': 'armeabi-v7a',
+         'update_version_code': '16209900',
+         'channel': 'tengxun_1128_0531',
+         'device_platform': 'android',
+         'iid': '2445779965058391',
+         'version_code': '160200',
+         'cdid': 'd535b60e-4324-4f42-94cd-7598fbdc3a78',
+         'is_android_pad': '0',
+         'openudid': 'b5850cc18df77aac',
+         'device_id': '1600500688229959',
+         'resolution': '720*1280',
+         'os_version': '5.1.1',
+         'language': 'zh',
+         'device_brand': 'samsung',
+         'aid': '1128',
+         'minor_status': '0',
+         'mcc_mnc': '46007'
+        },
+    }
+
+# logging
